55a78b47a1b524eda286213b6f96915cc181b1c2
sabarish-learning <66541812+sabarish-learning@users.noreply.github.com>
2020-06-12 09:50:01 +0530

    Update component.ts

--- component.ts
+++ component.ts
@@ -1,21 +1,49 @@
-export class AppComponent  {
+import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
   pname:string ='';
-   pcity:string='';
-  pstate:string='';
-  bname:string='';
-  bcity:string='';
-  bstate:string='';
+  pcity:string='';
+ pstate:string='';
+ bname:string='';
+ bcity:string='';
+ bstate:string='';
+ name:string ='';
+ city:string='';
+state:string='';
+cname:string='';
+ccity:string='';
+cstate:string='';
 filladdress($event){
+ if($event.target.checked== true){
+this.bname=this.pname;
+this.bcity=this.pcity;
+this.bstate=this.pstate;
+ }
+ else
+ {
+  this.bname="";
+  this.bcity="";
+  this.bstate="";
+ }
+ 
+}
+filladdress1($event){
   if($event.target.checked== true){
- this.bname=this.pname;
- this.bcity=this.pcity;
- this.bstate=this.pstate;
+ this.cname=this.name;
+ this.ccity=this.city;
+ this.cstate=this.state;
   
   }else
   {
-    this.bname="";
-    this.bcity="";
-    this.bstate="";
+    this.cname="";
+    this.ccity="";
+    this.cstate="";
   }
   
 }
